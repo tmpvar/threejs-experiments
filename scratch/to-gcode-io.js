9642dadfb3f89ad6b62beb51020e4946da8d7ded
Elijah Insua <tmpvar@gmail.com>
2014-03-06 14:56:41 -0800

    add code to send verts to gcode.io

---
+++ scratch/to-gcode-io.js
@@ -0,0 +1,39 @@
+window.exportToGcodeIO = function(url, geometry) {
+  url = url || 'http://gcode.io';
+  var w = window.open(url);
+
+  var send = function(obj) {
+    w.postMessage(JSON.stringify(obj), '*');
+  };
+
+  window.addEventListener('message', function(ev) {
+    if (ev.data === 'ready') {
+
+      // push the geometry through the pipe
+      for (var i=0; i<geometry.faces.length; i++) {
+        var face = geometry.faces[i];
+        var a = geometry.vertices[face.a];
+        var b = geometry.vertices[face.b];
+        var c = geometry.vertices[face.c];
+        send({
+          name : 'face',
+          data : {
+            verts : [
+              [a.x, a.y, a.z],
+              [b.x, b.y, b.z],
+              [c.x, c.y, c.z]
+            ],
+            normal: [face.normal.x, face.normal.y, face.normal.z]
+          }
+        });
+      }
+
+      send({ name: 'end'});
+    }
+  });
+
+
+
+  return w;
+
+};
